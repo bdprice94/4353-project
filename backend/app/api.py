9900dbe0cabc7f36a5d73a93628e4c6877a39bcd
Benjamin Price <ben.price2013@gmail.com>
2023-05-16 01:52:50 -0500

    Add argument parsing for backend launch, mess with static hosting

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+import os
 
 
 app = FastAPI()
@@ -16,7 +18,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
+# ideally there would be a proper place to store build artifacts for deployment
+# but for now this is just to mess around with since I'm bored
+build_dir = os.path.abspath('../frontend/build')
+app.mount("/static_test", StaticFiles(directory=build_dir), name="static_test")
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,21 @@
 import uvicorn
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="main",
+    description="Launches the backend server for the project",
+)
+parser.add_argument("-n", "--hostname", help="hostname of the server",
+                    default="0.0.0.0")
+parser.add_argument("-p", "--port", help="port number of the server",
+                    default="8000", type=int)
+parser.add_argument("-r", "--release", help="set flag if running in prod",
+                    action="store_true")
+args = parser.parse_args()
+
+hostname = args.hostname
+port = args.port
+release = args.release
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.api:app", host=hostname, port=port, reload=not release)
