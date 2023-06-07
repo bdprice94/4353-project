0c3345cad5e8c17fa6831d9d313b0878fd66ffc6
Andrew <andrew@andrewmbp.attlocal.net>
2023-06-06 21:08:29 -0500

    Create default directory if missing

--- backend/app/api.py
+++ backend/app/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
+from pathlib import Path
 
 
 app = FastAPI()
@@ -20,6 +21,7 @@ app.add_middleware(
 )
 # ideally there would be a proper place to store build artifacts for deployment
 # but for now this is just to mess around with since I'm bored
+Path("../frontend/build").mkdir(parents=True, exist_ok=True)
 build_dir = os.path.abspath('../frontend/build')
 app.mount("/static_test", StaticFiles(directory=build_dir), name="static_test")
 
