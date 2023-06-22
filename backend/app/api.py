a52fa1026fc75b7fc5229d6171370056300512f0
Andrew Chaleckis <achaleckis@gmail.com>
2023-06-22 08:49:21 -0500

    Simplify FastAPI
    We did not need a lot of the code. This code allows anyone to connect,
    and removes the weird test directory we didn't use.

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,29 +1,16 @@
-import os
-from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-
 
 app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
-# ideally there would be a proper place to store build artifacts for deployment
-# but for now this is just to mess around with since I'm bored
-Path("../frontend/build").mkdir(parents=True, exist_ok=True)
-build_dir = os.path.abspath('../frontend/build')
-app.mount("/static_test", StaticFiles(directory=build_dir), name="static_test")
 
 
 @app.get("/", tags=["root"])
