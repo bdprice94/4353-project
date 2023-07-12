ece3bdbb6c090843ffe7bbfddc01666be4c0d369
CAIRO-LAPTOP\cairo <cairoudobi2002@yahoo.com>
2023-07-12 17:49:37 -0500

    Clean up login and profile front end

--- frontend/src/LoginForm.tsx
+++ frontend/src/LoginForm.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import axios, {AxiosError} from 'axios';
 import { backendurl, setCookie } from './utils';
 import styles from "./LoginForm.module.css";
-
+import {useState} from 'react';
+import { Navigate } from "react-router-dom";
 
 
 const backendurl_users = `${backendurl}/users`; // will need to read from env if we need to host this
- 
+
+
 export interface UserLoginForm {
     username: { value: string },
     password:  { value: string },
@@ -29,47 +31,51 @@ const convertFormToModel = (form: UserLoginForm) => {
     }
 }
 
-const loginFormSubmit: React.FormEventHandler<HTMLFormElement> = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & UserLoginForm;
-    const userLoginForm = {
-        username: {value: target.username.value},
-        password: {value: target.password.value},
-    };
-    const userLogin = convertFormToModel(userLoginForm);
-    axios.post(`${backendurl_users}/login`, userLogin)
-        .then(response => {
-            console.log(response)
-            const data = response.data as UserLoginResponse
-            alert(`Successfully logged in ${data.username}!`);
-            setCookie('username', data.username);
-            setCookie('userid', data.id.toString())
-        })
-        .catch((e: AxiosError) => {
-            let errString = "Sorry, we don't know what happened. Please verify information is correct";
-            if ('response' in e && e.response !== undefined) {
-                if (e.response.status === 422) {
-                    const data = e.response.data as { detail: Array<string> };
-                    errString = data.detail
-                        .map((err: any) => err.msg)
-                        .join('\n');
-                }
-                else if (e.response.status === 404) {
-                    const data = e.response.data as { detail: string };
-                    errString = data.detail
-                }
-                else {
-                    console.log(e.response.data)
-                }
-            }
-            else {
-                console.log(e)
-            }
-            alert(errString)
-        });
-}
+
 
 const LoginForm: React.FunctionComponent = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const loginFormSubmit: React.FormEventHandler<HTMLFormElement> = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & UserLoginForm;
+        const userLoginForm = {
+            username: {value: target.username.value},
+            password: {value: target.password.value},
+        };
+        const userLogin = convertFormToModel(userLoginForm);
+        axios.post(`${backendurl_users}/login`, userLogin)
+            .then(response => {
+                console.log(response)
+                const data = response.data as UserLoginResponse
+                alert(`Successfully logged in ${data.username}!`);
+                setCookie('username', data.username);
+                setCookie('userid', data.id.toString());
+                setIsLoggedIn(true);
+            })
+            .catch((e: AxiosError) => {
+                let errString = "Sorry, we don't know what happened. Please verify information is correct";
+                if ('response' in e && e.response !== undefined) {
+                    if (e.response.status === 422) {
+                        const data = e.response.data as { detail: Array<string> };
+                        errString = data.detail
+                            .map((err: any) => err.msg)
+                            .join('\n');
+                    }
+                    else if (e.response.status === 404) {
+                        const data = e.response.data as { detail: string };
+                        errString = data.detail
+                    }
+                    else {
+                        console.log(e.response.data)
+                    }
+                }
+                else {
+                    console.log(e)
+                }
+                alert(errString)
+            });
+    }
     return (
         <form  className={styles.form} onSubmit={loginFormSubmit}>
             <label className={styles.label}>
@@ -81,6 +87,7 @@ const LoginForm: React.FunctionComponent = () => {
             </label>
             <br/>
             <input type='submit' id ='submit' ></input>
+            {isLoggedIn && <Navigate to="/user-profile-form" />} 
         </form>
     )
 }
